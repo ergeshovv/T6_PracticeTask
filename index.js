f9d196036a2bf005583c00128d5195c30fa55b68
Erbol <erbol.ergeshovv@gmail.com>
2021-10-02 01:40:19 +0600

    added files

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+
+let inputArr = fs.readFileSync('input.txt', 'utf-8').split("\r\n")
+// [ 'привет', 'как дела?', 'хорошо', 'салам', 'салют' ]
+let patternArr = fs.readFileSync('patterns.txt', 'utf-8').split("\r\n")
+// [ 'привет', 'хорошо', 'как дела?', 'салем', 'салют' ]
+
+console.log(patternArr)
+
+//                              ПЕРВЫЙ РЕЖИМ                                   //
+function getArraysIntersection(a1,a2){
+    return  a1.filter(function(n) { return a2.indexOf(n) !== -1;});
+}
+
+console.log(getArraysIntersection(inputArr, patternArr));
+
+
+//                               ВТОРОЙ РЕЖИМ                                  //
+function getArraysLineIntersection(a1,a2){
+    return  a1.filter(function(n) { return a2.indexOf(n) === a1.indexOf(n);});
+}
+
+console.log(getArraysLineIntersection(inputArr, patternArr))
